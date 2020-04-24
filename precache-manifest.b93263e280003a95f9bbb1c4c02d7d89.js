d7056cbd19c207ad077252ce802be25103d10262
Steven Mann <stevencmann2@gmail.com>
2020-04-24 14:31:01 -0500

    Updates

---
+++ precache-manifest.b93263e280003a95f9bbb1c4c02d7d89.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0287d9f16c1cefaf3327e577c48fee2",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "7a42d6f4212aa0ff7b11",
+    "url": "/React-Portfolio/static/css/2.69580acf.chunk.css"
+  },
+  {
+    "revision": "1ad5a677f3960b51dbb9",
+    "url": "/React-Portfolio/static/css/main.f9eaa5e5.chunk.css"
+  },
+  {
+    "revision": "7a42d6f4212aa0ff7b11",
+    "url": "/React-Portfolio/static/js/2.d146a5dd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Portfolio/static/js/2.d146a5dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad5a677f3960b51dbb9",
+    "url": "/React-Portfolio/static/js/main.c214c66e.chunk.js"
+  },
+  {
+    "revision": "5d16534d7724865583f3",
+    "url": "/React-Portfolio/static/js/runtime-main.2316dad8.js"
+  },
+  {
+    "revision": "e0f9254666a3fa591ac410a8f3c716af",
+    "url": "/React-Portfolio/static/media/API.e0f92546.png"
+  },
+  {
+    "revision": "3fc94d32aa69dd760bda0faadc04d042",
+    "url": "/React-Portfolio/static/media/Auth0.3fc94d32.png"
+  },
+  {
+    "revision": "a97ab1c98c61d26c0830db925eb5c239",
+    "url": "/React-Portfolio/static/media/Balcony.a97ab1c9.JPG"
+  },
+  {
+    "revision": "3ac4271da9cbce1f42faa75a809dbfc9",
+    "url": "/React-Portfolio/static/media/Bamazon.3ac4271d.png"
+  },
+  {
+    "revision": "64aaeb1642cfcfb42a075db1fe9994da",
+    "url": "/React-Portfolio/static/media/Bootstrap.64aaeb16.png"
+  },
+  {
+    "revision": "bae805f6d6b7d4f3bd51968ec5c53a11",
+    "url": "/React-Portfolio/static/media/Burger.bae805f6.jpg"
+  },
+  {
+    "revision": "a32f83aa2c675058e4a05a0fd4da05eb",
+    "url": "/React-Portfolio/static/media/CSS.a32f83aa.png"
+  },
+  {
+    "revision": "79bae3767dd6079143768dad71bebfe7",
+    "url": "/React-Portfolio/static/media/CedricLetsch.79bae376.jpg"
+  },
+  {
+    "revision": "f9bfff6062cf81124abdc0c0992c2b84",
+    "url": "/React-Portfolio/static/media/ENV.f9bfff60.png"
+  },
+  {
+    "revision": "d21b5da966416fbf4f80c14411df8d9f",
+    "url": "/React-Portfolio/static/media/Expo.d21b5da9.png"
+  },
+  {
+    "revision": "0bda9d2164af1e6902331ffccb32dd9c",
+    "url": "/React-Portfolio/static/media/FirebaseAuth.0bda9d21.png"
+  },
+  {
+    "revision": "20c3785a41e2da81df5646534e4704b6",
+    "url": "/React-Portfolio/static/media/FirebaseRealTime.20c3785a.png"
+  },
+  {
+    "revision": "3c3c006567103e47ebe0ca20b59e0ab2",
+    "url": "/React-Portfolio/static/media/Firestore.3c3c0065.png"
+  },
+  {
+    "revision": "08ec57224f5d2a9929ead8a51b22e6f8",
+    "url": "/React-Portfolio/static/media/Git.08ec5722.png"
+  },
+  {
+    "revision": "ec0355663d5c8ba7a8c65ed28cec4d76",
+    "url": "/React-Portfolio/static/media/HuguesDeBuyer.ec035566.jpg"
+  },
+  {
+    "revision": "0fb1be87d8b8bd634fa1b8279594c8c6",
+    "url": "/React-Portfolio/static/media/Jquery.0fb1be87.svg"
+  },
+  {
+    "revision": "8ecd4d8c0043faa8533e395568106a9d",
+    "url": "/React-Portfolio/static/media/LevelUp.8ecd4d8c.png"
+  },
+  {
+    "revision": "1f6ab9cb23d496c0962fb59c7ecce00e",
+    "url": "/React-Portfolio/static/media/Liri.1f6ab9cb.png"
+  },
+  {
+    "revision": "a363d0b97aa9d55d0a07dccf3fcedb11",
+    "url": "/React-Portfolio/static/media/NPM.a363d0b9.svg"
+  },
+  {
+    "revision": "ddc22e49459c84740fbf1eb36aa97680",
+    "url": "/React-Portfolio/static/media/NatalieChaney.ddc22e49.jpg"
+  },
+  {
+    "revision": "0aa945b03e542fcb4207e4c3b59a30ed",
+    "url": "/React-Portfolio/static/media/NewsScraper.0aa945b0.png"
+  },
+  {
+    "revision": "909568db8df68a327bf3d50f148f02b0",
+    "url": "/React-Portfolio/static/media/NodeLogo.909568db.svg"
+  },
+  {
+    "revision": "712bd50e92a6b456626c8b0fc04b00cd",
+    "url": "/React-Portfolio/static/media/PassportMobileIPad.712bd50e.png"
+  },
+  {
+    "revision": "e88c748e07987a8f1766d591427870e2",
+    "url": "/React-Portfolio/static/media/Postman.e88c748e.png"
+  },
+  {
+    "revision": "9aabb564344e9d1d0fd3458b5b3391d6",
+    "url": "/React-Portfolio/static/media/RNDebugger.9aabb564.png"
+  },
+  {
+    "revision": "d07e0169ac1a0e108252fc73185d71ce",
+    "url": "/React-Portfolio/static/media/RR.d07e0169.jpg"
+  },
+  {
+    "revision": "dd717418f1a83826bb5c57a99ceae268",
+    "url": "/React-Portfolio/static/media/ReactFirebase.dd717418.svg"
+  },
+  {
+    "revision": "c8f44df3b0bdbe3addb48058ed44c088",
+    "url": "/React-Portfolio/static/media/Redux.c8f44df3.png"
+  },
+  {
+    "revision": "76f89a7f6e94bed3f15484c4740af4f3",
+    "url": "/React-Portfolio/static/media/Robo3T.76f89a7f.png"
+  },
+  {
+    "revision": "87fa82d4db4ef68a6c9702d1f377fd6b",
+    "url": "/React-Portfolio/static/media/Scratch.87fa82d4.svg"
+  },
+  {
+    "revision": "4d93d4d245a7be5cd514224ab660d4e4",
+    "url": "/React-Portfolio/static/media/Steven-Mann-Resume.4d93d4d2.pdf"
+  },
+  {
+    "revision": "7e73d334f6ab498f09fa6861d4ab8e7a",
+    "url": "/React-Portfolio/static/media/VsCode.7e73d334.png"
+  },
+  {
+    "revision": "7f1669326fedcbe611ef9692ed163ec7",
+    "url": "/React-Portfolio/static/media/Workbench.7f166932.png"
+  },
+  {
+    "revision": "799cc44d05faa618f578724488d509b8",
+    "url": "/React-Portfolio/static/media/XcodeSim.799cc44d.png"
+  },
+  {
+    "revision": "419151b9773e2139f686cfb617e541b2",
+    "url": "/React-Portfolio/static/media/animal.419151b9.png"
+  },
+  {
+    "revision": "4e35a9ed5d29a183ac0cfa04695bc045",
+    "url": "/React-Portfolio/static/media/bassClickyGame.4e35a9ed.png"
+  },
+  {
+    "revision": "e6c03604a06c3fac402cfceb96e80048",
+    "url": "/React-Portfolio/static/media/books.e6c03604.jpg"
+  },
+  {
+    "revision": "ad4c28835fef09ba760559d5f6166c58",
+    "url": "/React-Portfolio/static/media/cablecar.ad4c2883.png"
+  },
+  {
+    "revision": "ed2f373e10b05199cfbc134da918f167",
+    "url": "/React-Portfolio/static/media/chartsJs.ed2f373e.svg"
+  },
+  {
+    "revision": "4368e8e44f07465ae52afcabeeb065fa",
+    "url": "/React-Portfolio/static/media/friends.4368e8e4.png"
+  },
+  {
+    "revision": "bc9e95fc4a3ea4998503a65683cdd37d",
+    "url": "/React-Portfolio/static/media/handle.bc9e95fc.png"
+  },
+  {
+    "revision": "334a35d353fed1484c6bd38f8cc240bb",
+    "url": "/React-Portfolio/static/media/html1.334a35d3.png"
+  },
+  {
+    "revision": "5257508524eeb31de2e4a9c323e96209",
+    "url": "/React-Portfolio/static/media/inquirer.52575085.svg"
+  },
+  {
+    "revision": "79046a553ce2e9ea7fc1992ede20e610",
+    "url": "/React-Portfolio/static/media/javascript.79046a55.png"
+  },
+  {
+    "revision": "2842498681360a4c0309bfa35464152d",
+    "url": "/React-Portfolio/static/media/map.28424986.png"
+  },
+  {
+    "revision": "ba0713ee1b8d9e90e0189226831227dd",
+    "url": "/React-Portfolio/static/media/moment.ba0713ee.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/React-Portfolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/React-Portfolio/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "c711a05b7defb1a148f8a13f0e1db524",
+    "url": "/React-Portfolio/static/media/netlify.c711a05b.svg"
+  },
+  {
+    "revision": "456966050e0d8e447aa434a5785f6c8e",
+    "url": "/React-Portfolio/static/media/passport.45696605.png"
+  },
+  {
+    "revision": "78ee9c8a6a9f322010a59e932bc83920",
+    "url": "/React-Portfolio/static/media/pattern.78ee9c8a.png"
+  },
+  {
+    "revision": "76cc0b3e416f0f02eb9ff8071306874c",
+    "url": "/React-Portfolio/static/media/retroDiner.76cc0b3e.jpg"
+  }
+]);
